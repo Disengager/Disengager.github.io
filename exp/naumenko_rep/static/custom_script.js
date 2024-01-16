06b059ebf34c77cb133134fc13e0cc9872b6675d
Disengager <disengagerAMV@gmail.com>
2024-01-16 16:51:53 +0300

    заменил время для высчитывания размера

--- exp/naumenko_rep/static/custom_script.js
+++ exp/naumenko_rep/static/custom_script.js
@@ -194,7 +194,7 @@ var sizes = {
 		return Math.abs(this.size_px)
 	},
 	stimules_size: function() {
-		return sizes.size_px / 14.384615384615385
+		return sizes.size_px / 11.333333333333334
 	}
 }
 
